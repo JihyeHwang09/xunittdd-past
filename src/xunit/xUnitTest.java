8293676f8f29c69d73c4ee44e9540670a1318cb8
jihyehwang09 <jihye.hwang@kurlycorp.com>
2021-06-01 12:44:31 +0900

    테스트를 실행하는 로직을 메서드로 추출하라
    * TestCase를 상속받는 TestCaseTest 클래스를 만들었습니다.
    * testRunning 메서드를 생성하여 테스트 실행 로직을 추출하였습니다.

--- src/xunit/xUnitTest.java
+++ src/xunit/xUnitTest.java
@@ -2,13 +2,6 @@ package xunit;
 
 public class xUnitTest {
   public static void main(String[] args) {
-    // WasRun이라는 테스트 클래스에 어느 테스트를 실행할 것인지 테스트 이름을 넘긴다
-    WasRun wasRun = new WasRun("testMethod");
-    // 이 테스트 메서드가 실행되었었는지 어떻게 검증할것인가?
-    // flag를 단다.
-    System.out.println(wasRun.wasRun); // false
-    wasRun.run();
-    System.out.println(wasRun.wasRun); // true
-
+    new TestCaseTest("testRunning").run();
   }
 }
